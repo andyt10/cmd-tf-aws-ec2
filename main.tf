b15ed158e53ef4cd1e0ade1c088a6465edb6d051
Andy Thompson <andy+github@vkandy.net>
2021-09-10 12:27:11 +1000

    testing

--- main.tf
+++ main.tf
@@ -87,7 +87,10 @@ resource "aws_instance" "main" {
       kms_key_id            = lookup(var.root_block_device, "kms_key_id", null)
       volume_size           = lookup(var.root_block_device, "volume_size", null)
       volume_type           = lookup(var.root_block_device, "volume_type", null)
-      tags = var.volume_tags != {}? null : merge(var.root_block_device_tags,
+      tags =  merge(
+        var.tags,
+        var.volume_tags,
+        var.root_block_device_tags,
         { Name = var.instance_name }
       )
     }
